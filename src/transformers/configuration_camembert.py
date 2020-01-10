c4e5e999a47558bc163090c0a4040a9c5a7a8eb1
Lysandre <lysandre.debut@reseau.eseo.fr>
2020-01-10 18:01:21 +0100

    Camembert + RoBERTa configuration updates

--- src/transformers/configuration_camembert.py
+++ src/transformers/configuration_camembert.py
@@ -30,6 +30,15 @@ CAMEMBERT_PRETRAINED_CONFIG_ARCHIVE_MAP = {
 
 class CamembertConfig(RobertaConfig):
     r"""
+        This is the configuration class to store the configuration of an :class:`~transformers.CamembertModel`.
+        It is used to instantiate an Camembert model according to the specified arguments, defining the model
+        architecture. Instantiating a configuration with the defaults will yield a similar configuration to that of
+        the BERT bert-base-uncased architecture.
+
+        Configuration objects inherit from  :class:`~transformers.PretrainedConfig` and can be used
+        to control the model outputs. Read the documentation from  :class:`~transformers.PretrainedConfig`
+        for more information.
+
         The :class:`~transformers.CamembertConfig` class directly inherits :class:`~transformers.BertConfig`.
         It reuses the same defaults. Please check the parent class for more information.
     """
