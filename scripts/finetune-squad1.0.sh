e1ff6661367376dab32a08436610d415d1d5c5f9
Son Nguyen <thanhson.study@gmail.com>
2020-01-12 20:37:58 +0700

    update

--- scripts/finetune-squad1.0.sh
+++ scripts/finetune-squad1.0.sh
@@ -10,6 +10,8 @@ EPOCHS=10
 MAX_LEN=384
 STRIDE=128
 GRAD_ACCU=12
+WARMUP=100
+WDECAY=1e-6
 
 export CUDA_VISIBLE_DEVICES=2,3
 NUM_GPUS=2
@@ -29,4 +31,6 @@ python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} \
 	--doc_stride ${STRIDE} \
 	--output_dir ${OUT_DIR} \
 	--per_gpu_train_batch_size ${BATCH_SIZE} \
-	--gradient_accumulation_steps ${GRAD_ACCU}
+	--gradient_accumulation_steps ${GRAD_ACCU} \
+	--warmup_steps ${WARMUP} \
+	--weight_decay ${WDECAY}
