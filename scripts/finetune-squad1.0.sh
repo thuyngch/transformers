1dba0e726293d379afac34e7e096c576554de17e
Son Nguyen <thanhson.study@gmail.com>
2020-01-12 09:42:20 +0700

    add scripts

---
+++ scripts/finetune-squad1.0.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+export SQUAD_DIR=/home/cybercore/thuync/datasets/SQuAD1.0
+export CUDA_VISIBLE_DEVICES=0,1
+
+python -m torch.distributed.launch --nproc_per_node=2 \
+	examples/run_squad.py \
+	--model_type bert \
+	--model_name_or_path bert-base-cased \
+	--do_train \
+	--do_eval \
+	--do_lower_case \
+	--train_file $SQUAD_DIR/train-v1.1.json \
+	--predict_file $SQUAD_DIR/dev-v1.1.json \
+	--learning_rate 3e-5 \
+	--num_train_epochs 2 \
+	--max_seq_length 384 \
+	--doc_stride 128 \
+	--output_dir ../models/wwm_uncased_finetuned_squad/ \
+	--per_gpu_train_batch_size 24 \
+	--gradient_accumulation_steps 12
